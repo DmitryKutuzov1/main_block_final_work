273bb123b63606240d6c7464f0885f640e9865c6
Dmitry <dimakutuzov1234@gmail.com>
2023-11-22 21:29:58 +0300

    Folder with solution added

---
+++ Task/Program.cs
@@ -0,0 +1,16 @@
+﻿string[] array = { "Hello", "2", "world", ":-)", "1234", "1567", "-2", 
+                    "computer science", "Russia", "Denmark", "Kazan" };
+int count = 0;
+for (int i = 0; i < array.Length; i++) {
+    if (array[i].Length <= 3) count++;
+}
+
+string[] result = new string[count];
+for (int i = 0; i < array.Length; i++) {
+    if (array[i].Length <= 3) {
+        result[count - 1] = array[i];
+        count--;
+    }
+}
+
+Console.Write($"{String.Join(", ", result)}");
